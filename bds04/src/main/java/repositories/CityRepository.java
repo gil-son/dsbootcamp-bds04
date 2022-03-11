95c45045765167c787d39dbe2edbd8b420881884
gilszon <gilszon@gmail.com>
2022-03-10 23:25:29 -0300

    bds04

--- bds04/src/main/java/repositories/CityRepository.java
+++
@@ -1,8 +0,0 @@
-package repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.devsuperior.bds04.entities.City;
-
-public interface CityRepository extends JpaRepository<City, Long>{
-
-}
--- bds04/src/main/java/repositories/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.devsuperior.bds04.entities.Event;
-
-public interface EventRepository extends JpaRepository<Event, Long>{
-
-}
